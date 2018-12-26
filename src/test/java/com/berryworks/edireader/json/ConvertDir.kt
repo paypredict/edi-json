1433ad45e3ae82bd0e0a66e6517196bea0564b50
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2018-12-26 14:35:43 +0500

    add ConvertDir.kt

---
+++ src/test/java/com/berryworks/edireader/json/ConvertDir.kt
@@ -0,0 +1,26 @@
+package com.berryworks.edireader.json
+
+import java.io.File
+import java.io.IOException
+
+/**
+ * <p>
+ * Created by alexei.vylegzhanin@gmail.com on 12/26/2018.
+ */
+fun main(args: Array<String>) {
+    val outDir =
+        File("com.berryworks.edireader.json")
+            .absoluteFile
+            .also { if (!it.isDirectory) throw IOException("Out directory $it not found") }
+
+    val ediToJson = EdiToJson()
+    for (srcFile in File(".").listFiles()) {
+        if (!srcFile.isFile) continue
+        val outFile = outDir.resolve(srcFile.name + ".json")
+        srcFile.reader().use { reader ->
+            outFile.writer().use { writer ->
+                ediToJson.asJson(reader, writer)
+            }
+        }
+    }
+}
